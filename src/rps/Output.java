03c25b25a7a837644b8f66c6936a8c9b7704e1a6
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2013-02-18 19:38:42 +0100

    *rps*: add Output type definition as Enum.

---
+++ src/rps/Output.java
@@ -0,0 +1,12 @@
+package rps;
+
+/**
+ * Primitive Type defining the output of a Rock-Paper-Scissors game.
+ * 
+ * @author Felix Ortmann
+ * 
+ */
+public enum Output
+{
+    Rock, Paper, Scisorrs;
+}
