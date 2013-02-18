800942f6f8025faf22fcf5fba1c158799de3bd58
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2013-02-18 21:55:49 +0100

    *rps*: add startup for the application.

---
+++ src/rps/Startup.java
@@ -0,0 +1,29 @@
+package rps;
+
+/**
+ * Start Up the Rock-Paper-Scissors Application with pre-set {@link Player}s
+ * according to the Justsoftware definitions.
+ * 
+ * @author Felix Ortmann
+ * 
+ */
+public class Startup
+{
+
+    public static void main(String[] args)
+    {
+        // Set up Player A, always draw Paper
+        Player player1 = new Player("Player A");
+        player1.setDrawOutput(Output.Paper);
+
+        // Set up Player B
+        Player player2 = new Player("Player B");
+
+        // Start the manager
+        GameManager manager = new GameManager();
+
+        // play 100 games like wanted by Justsoftware
+        manager.playMultipleGames(player1, player2, 100);
+
+    }
+}
